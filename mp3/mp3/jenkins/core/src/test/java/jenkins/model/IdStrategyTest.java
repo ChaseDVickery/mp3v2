679c5ae2cf5bcd41821e0cb658ed7c3150672f81
Chase Vickery <chase.vickery@uky.edu>
2020-10-28 22:31:37 -0400

    q4 (try6)

--- mp3/mp3/jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
+++ mp3/mp3/jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
-import IdStrategy.CaseSensitive;
+//import IdStrategy.CaseSensitive;
 
 public class IdStrategyTest {
 
