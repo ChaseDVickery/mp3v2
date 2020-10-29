8af704ad76d9544a9dfbe475c69d5a900e07278d
Chase Vickery <chase.vickery@uky.edu>
2020-10-28 22:20:40 -0400

    q4 (try3)

--- mp3/mp3/jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
+++ mp3/mp3/jenkins/core/src/test/java/jenkins/model/IdStrategyTest.java
@@ -7,21 +7,21 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.jvnet.hudson.test.Issue;
-import CaseSensitive;
+import CaseSensitive.idFromFilename;
 
 public class IdStrategyTest {
 
     @Test
     public void testidFromFilenameBasic() {
         String filename = "abcXYZ_. -";
-        String id = CaseInsensitive.idFromFilename(filename);
+        String id = CaseSensitive.idFromFilename(filename);
         assertEquals(filename, id);
     }
 
     @Test
     public void testidFromFilename() {
         String filename = "abc~a";
-        String id = CaseInsensitive.idFromFilename(filename);
+        String id = CaseSensitive.idFromFilename(filename);
         assertEquals(id, "abcA");
     }
 }
